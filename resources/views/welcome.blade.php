8bdb8a1bba4e3b1bb4a3a084339aef1a12c1e9a2
unknown <ghoftriki@gmail.com>
2024-08-01 09:38:33 +0100

    qr 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -346,37 +346,29 @@ document.getElementById('prev').onclick = function() {
     <!-- Vesitable Shop End -->
 
 
-    <!-- Banner Section Start-->
-    <div class="container-fluid banner bg-secondary my-5">
-        <div class="container py-5">
-            <div class="row g-4 align-items-center">
-                <div class="col-lg-6">
-                    <div class="py-4">
-                        <h1 class="display-3 text-white">Fresh Exotic Fruits</h1>
-                        <p class="fw-normal display-3 text-dark mb-4">in Our Store</p>
-                        <p class="mb-4 text-dark">The generated Lorem Ipsum is therefore always free from repetition
-                            injected humour, or non-characteristic words etc.</p>
-                        <a href="#"
-                            class="banner-btn btn border-2 border-white rounded-pill text-dark py-3 px-5">BUY</a>
-                    </div>
+   <!-- Banner Section Start -->
+   <div class="container-fluid banner bg-lightblue my-5">
+    <div class="container py-5">
+        <div class="row g-4 align-items-center">
+            <!-- Text Section -->
+            <div class="col-lg-6">
+                <div class="py-4 text-center text-lg-start">
+                    <h1 class="display-3 text-pink">Testez Avant d'Acheter !</h1>
+                    <p class="fw-normal text-pink mb-4">Scannez le QR code pour essayer nos bijoux en réalité augmentée et participez à notre concours pour gagner des cadeaux exclusifs.</p>
+                    <p class="text-pink mb-4">Partagez votre look avec #FimoChicTest pour tenter de gagner des prix !</p>
+                    <a href="#" class="banner-btn btn border-2 border-pink rounded-pill text-pink py-3 px-5">Scannez le QR Code</a>
                 </div>
-                <div class="col-lg-6">
-                    <div class="position-relative">
-                        <img src="img/baner-1.png" class="img-fluid w-100 rounded" alt="">
-                        <div class="d-flex align-items-center justify-content-center bg-white rounded-circle position-absolute"
-                            style="width: 140px; height: 140px; top: 0; left: 0;">
-                            <h1 style="font-size: 100px;">1</h1>
-                            <div class="d-flex flex-column">
-                                <span class="h2 mb-0">50$</span>
-                                <span class="h4 text-muted mb-0">kg</span>
-                            </div>
-                        </div>
-                    </div>
+            </div>
+            <!-- QR Code Section -->
+            <div class="col-lg-6 text-center">
+                <div class="position-relative">
+                    <img src="img/qrfimo.png" class="img-fluid rounded" alt="QR Code">
                 </div>
             </div>
         </div>
     </div>
-    <!-- Banner Section End -->
+</div>
+<!-- Banner Section End -->
 
 
     <!-- Bestsaler Product Start -->
